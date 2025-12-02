969f976f563699286f9513e7356641b49cc3b2c0
LAPTOP-S2OT7OKK\czxroy <czxroy@163.com>
2025-12-02 18:27:44 +0800

    更新初始化脚本

--- docker/init-db.sql
+++ docker/init-db.sql
@@ -16,7 +16,6 @@ SET search_path TO public;
 -- 这里只是作为文档记录
 
 -- 性能优化设置
-ALTER SYSTEM SET shared_preload_libraries = '';
 ALTER SYSTEM SET max_connections = 200;
 ALTER SYSTEM SET shared_buffers = '256MB';
 ALTER SYSTEM SET effective_cache_size = '1GB';
